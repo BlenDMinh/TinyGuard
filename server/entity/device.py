09a8ded5bbf7e27c6d63afa3e7f6c01764716600
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-10-28 11:49:14 +0700

    refractor

--- server/entity/device.py
+++ server/entity/device.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 import config
 
@@ -9,3 +9,5 @@ db = config.db
 
 class Device(db.Model):
     code: Mapped[str] = mapped_column(String, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="devices")
--- server/entity/user.py
+++ server/entity/user.py
@@ -1,6 +1,7 @@
+from typing import List
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 import config
 
@@ -13,3 +14,4 @@ class User(db.Model):
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     age: Mapped[int] = mapped_column(Integer)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    devices: Mapped[List["Device"]] = relationship(back_populates='user')
