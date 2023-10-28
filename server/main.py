f530da5410275bbc327ba5d5b4203bf3ed8c6fe0
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-10-28 10:32:33 +0700

    SQLAlchemy db

--- server/main.py
+++ server/main.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
 
 app = Flask("Baby recognition")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 
 if __name__ == "__main__":
     from config import config
@@ -8,6 +18,7 @@ if __name__ == "__main__":
     for url, handler in route.items():
         app.add_url_rule(
             url, endpoint=handler["endpoint"], view_func=handler["view"], methods=handler["methods"])
+
     app.run(
         debug=config["debug"],
         port=config["port"],
