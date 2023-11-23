29c3461e4d1fd9ad5002231a3ebf52d8b61a4249
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-11-23 18:15:45 +0700

    Device

---
+++ client/tinyguard/lib/data/repository/device_repository.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:tinyguard/data/datasource/remote/entity/device_entity.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:tinyguard/data/datasource/remote/entity/image_predict_entity.dart';
+import 'package:tinyguard/flavor_config.dart';
+
+class Device {
+  String code;
+  IO.Socket _isocket;
+  IO.Socket _asocket;
+  final image_predicts = StreamController<ImagePredict>();
+
+  Device(this.code) : 
+    _isocket = IO.io("${FlavorConfig.instance.baseApiUrl}/${code}_i", 
+    IO.OptionBuilder().setTransports(['websocket']).build()),
+    _asocket = IO.io("${FlavorConfig.instance.baseApiUrl}/${code}_a") {
+      debugPrint("${FlavorConfig.instance.baseApiUrl}/${code}_i");
+      _isocket.connect();
+      _asocket.connect();
+      _isocket.on('connect', (data) {
+        debugPrint("connected");
+      });
+      _isocket.on("ImagePrediction", (data) {
+        image_predicts.sink.add(ImagePredict.fromJson(data));
+        debugPrint(data.toString());
+      });
+    }
+
+  factory Device.fromEntity(DeviceEntity entity) {
+    return Device(entity.code);
+  }
+}
