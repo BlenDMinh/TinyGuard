04c7c08d04bcde0523ac92da0711dc1014c16592
Trương Công Hoàng Long <truongconghoanglong@gmail.com>
2023-11-23 17:39:05 +0700

    audio predict

--- server/view/device_api.py
+++ server/view/device_api.py
@@ -4,10 +4,15 @@ from flask import jsonify, request, Response
 from flask_socketio import emit, send
 from config import socketio
 from container import container, Component, Event
-from model.predict_model import ImagePredict
+from model.predict_model import ImagePredict, AudioPredict
 from service.device_service import DeviceService
 from service.models import WrapResponseDto
-
+import torchaudio
+from io import BytesIO
+import tempfile
+import os
+import torch
+import numpy
 
 
 def image_input():
@@ -17,10 +22,11 @@ def image_input():
             headers={
                 "Content-Type": "application/json"
             },
-            response=json.dumps(WrapResponseDto.error("Bad request", "imageFile is missing").to_json()),
+            response=json.dumps(WrapResponseDto.error(
+                "Bad request", "imageFile is missing").to_json()),
             status=HTTPStatus.BAD_REQUEST
         )
-    
+
     device_service: DeviceService = container.get(Component.DeviceService)
     prediction: ImagePredict = device_service.predict_image(image)
     emit(Event.ImagePrediction, prediction.to_json(),
@@ -29,11 +35,22 @@ def image_input():
         headers={
             "Content-Type": "application/json"
         },
-        response=json.dumps(WrapResponseDto.success(prediction.to_json(), "Successfully").to_json()),
+        response=json.dumps(WrapResponseDto.success(
+            prediction.to_json(), "Successfully").to_json()),
         status=HTTPStatus.OK
     )
 
+
 def audio_input():
     audio = request.files.get('audioFile')
-    audio.save('audio.wav')
-    return Response("OK")
+    audio = BytesIO(audio.read())
+    audio, sr = torchaudio.load(audio)
+    prediction = AudioPredict(wavform=audio).to_json()
+    print(prediction)
+    return Response(
+        headers={
+            "Content-Type": "application/json"
+        },
+        response=WrapResponseDto.success(prediction, "Sucessfully").to_json(),
+        status=HTTPStatus.OK
+    )
